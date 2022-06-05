148dadcdf770faa0117a67177c321bce1b5a2c91
soumesh6622 <soumesh6622@gmail.com>
2022-06-05 11:50:54 +0530

    added public constructor game

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,7 @@ import java.util.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.Graphics;
 
 public class Game extends JPanel implements KeyListener, ActionListener {
 
@@ -26,6 +27,18 @@ public class Game extends JPanel implements KeyListener, ActionListener {
     // Ball Direction
     private int ballXdir = -1;
     private int ballYdir = -2;
+
+    public Game() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        time = new Timer(delay, this);
+        time.start();
+    }
+
+    public void paint(Graphics g) {
+        
+    }
 
 
     @Override
