5de1b24ecbd5f06defb61e299cf5a48f370c5af9
soumesh6622 <soumesh6622@gmail.com>
2022-06-05 12:54:12 +0530

    added key press logic

--- src/Game.java
+++ src/Game.java
@@ -55,7 +55,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        // TODO Auto-generated method stub
+            }
         
     }
 
