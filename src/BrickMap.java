e1316709960d1acb901f52a4b5c7cbde7797929f
soumesh6622 <soumesh6622@gmail.com>
2022-06-05 13:44:02 +0530

    added code to draw the map

--- src/BrickMap.java
+++ src/BrickMap.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class BrickMap {
     public int map[][];
     public int brickWidth;
@@ -11,5 +13,31 @@ public class BrickMap {
                 map[i][j] = 1;
             }
         }
+
+        brickWidth = 540/c; // column
+        brickHeight = 150/r; // row
+    }
+
+    public void draw(Graphics2D g) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] > 0) {
+                    g.setColor(Color.MAGENTA);
+                    g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+
+                    // if we want to run the game even if the bricks
+                    // are gone, we have to use BasicStroke()
+                    // it defines a basic set of rendering attributes
+
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.black);
+                    g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+                }
+            }
+        }
+    }
+
+    public void setBrickValue(int value, int row, int col) {
+        map[row][col] = value;
     }
 }
