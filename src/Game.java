3a5228b43d7b4c31f708719b8acacbeed73b6ef3
soumesh6622 <soumesh6622@gmail.com>
2022-06-05 12:52:20 +0530

    correction to imports

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,6 @@
 import javax.swing.JPanel;
 import java.awt.event.KeyListener;
-import java.util.Timer;
+import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
